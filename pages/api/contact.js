c5d465014f07fa1115dbc5db0ec92dfe0c629229
DevLikeMike <mhall.project@gmail.com>
2022-02-04 09:53:19 -0500

    fixed email service provider issues

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,48 +1,50 @@
-import nodemailer from "nodemailer";
+// Lib requires
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+// Set API key
+let apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.EMAILAPI;
+
+// Set instance
+let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+// Declarations for smtp
+let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
 export default async (req, res) => {
-  // Check to make sure method is post
+  // Check method to be POST
   if (req.method === "POST") {
-    // Destruct the body information
-    const { firstName, lastName, email, phone, message } = req.body;
+    // Destruct the request body information
+    const { name, email, message, subject } = req.body;
 
-    // Build transporter for nodemailer (Boilerplate)
-    const transporter = nodemailer.createTransport({
-      // For gmail only
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      // Env variables
-      auth: {
-        user: process.env.user,
-        pass: process.env.pass,
-      },
-    });
-
-    // Try-Catch for attempt to send email
     try {
-      const emailRes = await transporter.sendMail({
-        from: email,
-        to: "caseyhalllaw@gmail.com",
-        subject: `Contact form submission from ${email}`,
-        html: `<p>New Form Submission from your site</p>
-        <p><strong>First Name: </strong> ${firstName} </p>
-        <p><strong>Last Name: </strong> ${lastName} </p>
-        <p><strong>Phone: </strong> ${phone} </p>
-        <p><strong>Message: </strong> ${message} </p>
-  
-        `,
-      });
+      // Build the email
+      sendSmtpEmail.subject = `${subject}`;
+      sendSmtpEmail.htmlContent = `<html><body><p>${message}<p></body></html>`;
+      sendSmtpEmail.sender = { name: `${name}`, email: `${email}` };
+      sendSmtpEmail.to = [{ email: `${process.env.user}` }];
 
-      // Confirm email sent
-      console.log("New Message Sent");
-    } catch (err) {
-      // Log error if message cannot send
-      console.log(err);
+      // Await the sendEmail function
+      const emailRes = await apiInstance.sendTransacEmail(sendSmtpEmail).then(
+        function (data) {
+          console.log(
+            "API called successfully. Returned data: " + JSON.stringify(data)
+          );
+
+          // Status response if successfully sent
+          res.status(200).json({ message: `Message Sent` });
+        },
+        function (error) {
+          console.error(error);
+
+          // Status response for bad request
+          res.status(400).json({ message: "Bad Response" });
+        }
+      );
+    } catch (error) {
+      console.log(error);
     }
-
-    // Status
-    res.status(200).json(req.body);
   } else {
     // Only allow post methods
     res.setHeader("Allow", ["POST"]);
